d728081cb99257fdad9e8d7b9077758fc1b0ea4a
鈴木　理 <o.schellenbaum.2021@gmail.com>
2025-07-13 17:12:55 +0900

    2つのテーブルにまたがった操作が必要なためiddItemメソッドを分割

--- src/main/java/com/example/app/mapper/ItemMapper.java
+++ src/main/java/com/example/app/mapper/ItemMapper.java
@@ -12,8 +12,9 @@ public interface ItemMapper {
 	//  商品一覧表示
 	public List<Item> showItemList();
 	
-	//	商品追加
-	public void addItem(String itemNumber, String itemName, Integer costPrice, Integer sellingPrice) ;
+	//	商品追加(販売価格の初期値も同時登録)
+	public void addItem(Item item) ;
+	public void addItemSelling(Item item) ;
 	
 	//	商品取消
 	public void deleteItem(String itemNumber);
