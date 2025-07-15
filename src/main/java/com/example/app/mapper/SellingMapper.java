135f9f856ad2ffaceaf32b1b39faa6ba229a5721
鈴木　理 <o.schellenbaum.2021@gmail.com>
2025-07-15 22:15:32 +0900

    販売登録の機能を実装

--- src/main/java/com/example/app/mapper/SellingMapper.java
+++ src/main/java/com/example/app/mapper/SellingMapper.java
@@ -2,16 +2,15 @@ package com.example.app.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.app.dto.Selling;
+
 @Mapper
 public interface SellingMapper {
 	
 	//	販売状況の追加
-	public void addStatus(String status);
+	public void addStatusToDatabase(Selling selling);
 	
 	//	販売状況の変更
-	public void changeStatus();
-	
-	//	販売状況の取消
-	public void deleteStatus();
+	public void changeStatus(Selling selling);
 	
 }
