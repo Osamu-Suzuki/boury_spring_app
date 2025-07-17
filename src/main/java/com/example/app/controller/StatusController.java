b47801249ad8a1571e60b364fad25d6f4a486f7a
鈴木　理 <o.schellenbaum.2021@gmail.com>
2025-07-17 21:33:58 +0900

    販売状況の管理機能実装と仮ページ用意

--- src/main/java/com/example/app/controller/StatusController.java
+++ src/main/java/com/example/app/controller/StatusController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.app.dto.Status;
 import com.example.app.mapper.StatusMapper;
@@ -20,14 +20,14 @@ public class StatusController {
 	public String addStatus(Model model,
 		@ModelAttribute Status status) {
 		statusMapper.addStatusToDatabase(status);
-		
 		//	リダイレクト：一覧表示ページへ
 		return "redirect:/list";
 	}
 	
 	@PostMapping("/deletestatus")
-	public String deleteStatus() {
-		
+	public String deleteStatus(Model model,
+		@RequestParam String itemNo) {
+		statusMapper.deleteStatus(itemNo);
 		//	リダイレクト：一覧表示ページへ
 		return "redirect:/list";
 	}
@@ -35,8 +35,7 @@ public class StatusController {
 	@PostMapping("/changestatus")
 	public String changeStatus(Model model,
 		@ModelAttribute Status status) {
-		statusMapper.changeStatus(status);
-		
+		statusMapper.changeStatus(status);	
 		//	リダイレクト：一覧表示ページへ
 		return "redirect:/list";
 	}
--- src/main/java/com/example/app/mapper/StatusMapper.java
+++ src/main/java/com/example/app/mapper/StatusMapper.java
@@ -10,6 +10,9 @@ public interface StatusMapper {
 	//	販売状況の追加
 	public void addStatusToDatabase(Status status);
 	
+	//	販売状況の削除
+	public void deleteStatus(String itemNo);
+	
 	//	販売状況の変更
 	public void changeStatus(Status status);
 	
